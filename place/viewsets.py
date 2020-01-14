730485a8dc44e89119d3c8ff7c14d6833704d109
Alexander Perechnev <aperechnev@icloud.com>
2020-01-14 20:20:04 +0200

    Cities autocompletion.

--- place/viewsets.py
+++ place/viewsets.py
@@ -6,3 +6,10 @@ from .serializers import CitySerializer, City
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+
+    def get_queryset(self):
+        query = self.request.query_params.get('q', None)
+        if query is None:
+            return self.queryset
+        
+        return self.queryset.filter(title__istartswith=query)
